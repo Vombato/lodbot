b178c2138fa1476524ea3309f225e2c32cf67f52
Matteo Joliveau <matteojoliveau@gmail.com>
2017-06-27 12:58:35 +0200

    Implementing rest api

--- backend/backend.rb
+++ backend/backend.rb
@@ -1,4 +1,32 @@
 require "sinatra"
+require "sinatra/namespace"
+require 'json'
+
+namespace "/api" do
+	namespace "/v1" do
+			post "/login" do
+				content_type :json
+				payload = JSON.parse(request.body.read)
+				res = {}
+
+				if payload.key? "username"
+					res[:username] = payload["username"]
+				else
+					status = "username missing"
+				end
+
+				if payload.key? "password"
+					res[:password] = payload["password"]
+				else
+					status = "password missing"
+				end
+
+				res[:status] = status
+
+				res.to_json
+			end
+	end
+end
 
 get "/" do
 	send_file "public/index.html"
